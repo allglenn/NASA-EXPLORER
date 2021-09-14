230f86153de6908d706518a07e4f9e800f966dd2
glennlenormand <glenn.allogho@supli.fr>
2021-09-15 00:07:35 +0200

    your commit

--- server/src/routes/planets/planets.router.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const planetsRouter = express.Router();
-const {getAllPlanets} = require("./planets.controller");
-
-planetsRouter.get("/ ",getAllPlanets);
-
-module.exports = planetsRouter
